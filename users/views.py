301f9896f8c6013fc4668283673316fdf29e8fb3
kpsingh13 <kamalpreet2011mms@gmail.com>
2024-09-20 17:07:14 +0530

    User registration module added

--- users/views.py
+++ users/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserChangeForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 # Create your views here.
+
+
 def register(request):
-    form=UserChangeForm()
-    return render(request,'user/register.html',{'form': form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form=UserCreationForm()
+
+
+    return render(request,'users/register.html',{'form': form})
+
+
+# message.debug
+# message.info
+# message.success
+# message.warning
+# message.error
